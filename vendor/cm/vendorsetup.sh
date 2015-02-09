53876a833fba244ba00c0f709525ce2116547470
surdupetru <surdu_petru@yahoo.com>
2015-02-09 22:37:59 +0100

    add add_lunch_combo cm_hwp7-userdebug

---
+++ vendor/cm/vendorsetup.sh
@@ -0,0 +1,5 @@
+for combo in $(curl -s https://raw.github.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-11.0 | awk {'print $1'})
+do
+    add_lunch_combo $combo
+    add_lunch_combo cm_hwp7-userdebug
+done
